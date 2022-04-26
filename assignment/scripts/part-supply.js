c284506af48e3efd52ad9e2edc8c9744da3bb99a
Andrew Leck <Aleck360@gmail.com>
2022-04-26 02:14:30 -0500

    completed questions 1-4

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -1,24 +1,37 @@
 console.log('****** Part Supply *******');
 // REQUIRED FEATURES
-
+partsNeeded = 40;
 // 1. Create a variable called 'partsNeeded' with a value of the number 40 
 //    & console.log the variable
-console.log('1. Number of partsNeeded:');
+console.log('1. Number of partsNeeded:', partsNeeded);
+
+
+supplyChanges = [ '3', '5', '-6', '0', '7', '11'];
 
 // 2. Create a variable call 'supplyChanges' set it to an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11
-console.log('2. Array of supplyChanges:');
+console.log('2. Array of supplyChanges:', supplyChanges);
+
+
 
 // 3. Console log the value of the second item in the 'supplyChanges' array
-console.log('3. Second supplyChange is:');
+console.log('3. Second supplyChange is:', [-6]);
 
+
+supplyChanges.pop();
 // 4. The last item was added by mistake. Remove it from the 'supplyChanges' 
 //    array & console.log the value removed.
-console.log('4. Removed item:');
+console.log('4. Removed item:',[11]);
 
+
+supplyChanges.push(25);
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Adding 25 to supplyChanges.');
 
+x = supplyChanges;
+for(i=0; i< x; i++){
+    console.log('yum')
+}
 // 6. Write a `for` loop that shows each value in the 'supplyChanges' array
 //    Use a console.log formatted as follows, where x is the value from the array
 //    - if it is a positive number (greater than 0), log 'Added x parts.' 
